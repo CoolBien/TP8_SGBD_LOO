d205a47317a0575c547fe0588f10ee468be435e6
Sami El Idrissi <elidrissi.sami89@gmail.com>
2023-12-07 11:34:10 +0100

    id à la noix

--- TP8_SGBD_LOO/src/bdd/data/Evaluation.java
+++ TP8_SGBD_LOO/src/bdd/data/Evaluation.java
@@ -2,6 +2,8 @@ package bdd.data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -10,6 +12,11 @@ import javax.persistence.Table;
 
 public class Evaluation {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
 	@Column(name = "note")
 	private int note;
 	
@@ -31,4 +38,13 @@ public class Evaluation {
 	public void setEnseignant(Enseignant enseignant) {
 		this.enseignant = enseignant;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
