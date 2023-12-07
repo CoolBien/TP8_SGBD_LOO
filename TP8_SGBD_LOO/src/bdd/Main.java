3fdb9071807da1eda3d1ff1d209150b74b208b68
Sami El Idrissi <elidrissi.sami89@gmail.com>
2023-12-07 11:45:55 +0100

    Du nouveau dans le main

--- TP8_SGBD_LOO/src/bdd/Main.java
+++ TP8_SGBD_LOO/src/bdd/Main.java
@@ -3,22 +3,20 @@ package bdd;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
-import org.eclipse.swt.widgets.TabItem;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import bdd.ui.EtudiantView;
 import bdd.util.HibernateUtil;
 
 public class Main {
 	public static void main(final String[] args) throws Exception {
 
-		try (
-			final SessionFactory sessFact = HibernateUtil.getSessionFactory();
-			final Session session = sessFact.getCurrentSession();
-		) {
+		try (final SessionFactory sessFact = HibernateUtil.getSessionFactory();
+				final Session session = sessFact.getCurrentSession();) {
 			System.out.println("Connecté");
 
 			final Display display = new Display();
@@ -28,12 +26,7 @@ public class Main {
 			final TabFolder tabFolder = new TabFolder(shell, SWT.TOP);
 			tabFolder.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-			final TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
-			tabItem.setText("Etudiant");
-
-
-
-			tabItem.setControl(composite);
+			new EtudiantView(tabFolder);
 
 			shell.pack();
 			shell.open();
--- TP8_SGBD_LOO/src/bdd/ui/EtudiantView.java
+++ TP8_SGBD_LOO/src/bdd/ui/EtudiantView.java
@@ -4,14 +4,21 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 
 public class EtudiantView {
 
-	public EtudiantView() {
+	public EtudiantView(TabFolder tabFolder) {
+
+		final TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
+		tabItem.setText("Etudiant");
+
 
 		final Composite composite = new Composite(tabFolder, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		composite.setLayout(new GridLayout(1, false));
+		tabItem.setControl(composite);
 	}
 
 }
