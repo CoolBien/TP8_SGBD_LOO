ed65cabd4a5c30f39353aaa804e20a818df10fb7
Zetrypio <mdemrtl@gmail.com>
2023-12-20 15:16:24 +0100

    Faut que eje push

--- TP_9_SGBD_LOO/src/bdd/Main.java
+++ TP_9_SGBD_LOO/src/bdd/Main.java
@@ -1,22 +1,100 @@
 package bdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import bdd.cli.UserView;
 import bdd.util.Utils;
 
 public class Main {
 
-	public static void main(final String[] args) {
+	public Main(final Connection conn) throws SQLException {
 		while (true) {
 			final String answer = Utils.prompt("Êtes vous un [M]édecin ou un [U]tilisateur ?").toLowerCase();
 			if (answer.startsWith("M")) {
 				// TODO
 			} else if(answer.startsWith("U")) {
-				new UserView();
+				new UserView(conn);
 				return;
+			} else if (answer.startsWith("R")) {
+				resetDataBase(conn);
 			} else if (answer.isBlank()) {
 				return;
 			}
 			System.err.println("Choix incorrect.");
+		}
+	}
+
+	private void resetDataBase(final Connection conn) throws SQLException {
+		final Statement statement = conn.createStatement();
+		statement.execute("DROP DATABASE IF EXISTS medecin;");
+		statement.execute("CREATE DATABASE medecin;");
+		statement.execute("USE medecin;");
+		statement.execute("CREATE TABLE Medecin(\r\n"
+				+ "   ID_Medecin INT,\r\n"
+				+ "   Nom VARCHAR(50) NOT NULL,\r\n"
+				+ "   Prenom VARCHAR(50),\r\n"
+				+ "   Num_Securite_Sociale INT,\r\n"
+				+ "   Salaire CURRENCY,\r\n"
+				+ "   PRIMARY KEY(ID_Medecin)\r\n"
+				+ ");");
+		statement.execute("CREATE TABLE Utilisateur(\r\n"
+				+ "   ID_Utilisateur INT,\r\n"
+				+ "   Nom VARCHAR(50),\r\n"
+				+ "   Prenom VARCHAR(50),\r\n"
+				+ "   Num_Securite_Sociale INT,\r\n"
+				+ "   PRIMARY KEY(ID_Utilisateur)\r\n"
+				+ ");");
+		statement.execute("CREATE TABLE Type_d_analyse(\r\n"
+				+ "   ID_Type_Analyse INT,\r\n"
+				+ "   Hemogramme VARCHAR(50),\r\n"
+				+ "   Groupe_Sanguin VARCHAR(3),\r\n"
+				+ "   Vitesse_de_sedimentation DOUBLE,\r\n"
+				+ "   PRIMARY KEY(ID_Type_Analyse)\r\n"
+				+ ");");
+		statement.execute("CREATE TABLE Reservation(\r\n"
+				+ "   ID_Reservation_ INT,\r\n"
+				+ "   Date_debut DATETIME,\r\n"
+				+ "   Date_fin DATETIME,\r\n"
+				+ "   Prix_a_payer CURRENCY,\r\n"
+				+ "   Prix_deja_paye CURRENCY,\r\n"
+				+ "   ID_Medecin INT NOT NULL,\r\n"
+				+ "   ID_Type_Analyse INT NOT NULL,\r\n"
+				+ "   ID_Utilisateur INT NOT NULL,\r\n"
+				+ "   PRIMARY KEY(ID_Reservation_),\r\n"
+				+ "   FOREIGN KEY(ID_Medecin) REFERENCES Medecin(ID_Medecin),\r\n"
+				+ "   FOREIGN KEY(ID_Type_Analyse) REFERENCES Type_d_analyse(ID_Type_Analyse),\r\n"
+				+ "   FOREIGN KEY(ID_Utilisateur) REFERENCES Utilisateur(ID_Utilisateur)\r\n"
+				+ ");");
+		statement.execute("\r\n"
+				+ "CREATE TABLE Autorisation(\r\n"
+				+ "   ID_Medecin INT,\r\n"
+				+ "   ID_Type_Analyse INT,\r\n"
+				+ "   PRIMARY KEY(ID_Medecin, ID_Type_Analyse),\r\n"
+				+ "   FOREIGN KEY(ID_Medecin) REFERENCES Medecin(ID_Medecin),\r\n"
+				+ "   FOREIGN KEY(ID_Type_Analyse) REFERENCES Type_d_analyse(ID_Type_Analyse)\r\n"
+				+ ");");
+	}
+
+	public static void main(final String[] args) {// Trouver la classe
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (final ClassNotFoundException e) {
+			System.err.println("Impossible de trouver la classe Driver pour MySql");
+			e.printStackTrace();
+			return;
+		}
+
+		// Connexion
+		try (
+			final Connection conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1/tp", "root", "");
+		) {
+			new Main(conn);
+		} catch (final SQLException e) {
+			System.err.println("Erreur de connexion ou autre");
+			e.printStackTrace();
 		}
 	}
 }
--- TP_9_SGBD_LOO/src/bdd/cli/UserView.java
+++ TP_9_SGBD_LOO/src/bdd/cli/UserView.java
@@ -1,9 +1,62 @@
 package bdd.cli;
 
+import java.sql.Connection;
+
+import bdd.data.Utilisateur;
+import bdd.util.Utils;
+
 public class UserView {
 
-	public UserView() {
+	private final Connection c;
+
+	public UserView(final Connection c) {
+		this.c = c;
 		System.out.println("Vous êtes un utilisateur.");
+		while (true) {
+			System.out.println("Que voulez-vous faire ?");
+			System.out.println(" - [C] Se connecter");
+			System.out.println(" - [I] S'inscrire");
+			final String answer = Utils.prompt(">").toLowerCase();
+			switch (answer.charAt(0)) {
+			case 'C' -> {
+				connexion();
+				return;
+			}
+			case 'I' -> {
+				inscription();
+				return;
+			}
+			default -> {
+				System.err.println("Mauvais choix");
+			}
+			}
+		}
+	}
+
+	private void inscription() {
+		System.out.println("Veuillez renseigner");
+		String answer = "";
+		String name = "";
+		String firstName = "";
+		int ssn = 0;
+		while (answer.isBlank()) {
+			answer = Utils.prompt("N° Sécu social: ").toLowerCase();
+			try { ssn = Integer.parseInt(answer);}
+			catch (final Exception e) {answer="";}
+		}
+		while (name.isBlank()) {
+			name = Utils.prompt("Nom: ").toLowerCase();
+		}
+		while (firstName.isBlank()) {
+			firstName = Utils.prompt("Prénom: ").toLowerCase();
+		}
+		final Utilisateur user = new Utilisateur(0, name, firstName, ssn);
+		c.prepareStatement("INSERT INTO User")
+	}
+
+	private void connexion() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
