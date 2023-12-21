e7c45a9a9562dfd1ca3230f524d0cddacac18680
CoolBien <elidrissi.sami89@gmail.com>
2023-12-21 11:48:37 +0100

    oui

--- TP10_SGBD_LOO/src/bdd/view/ConnexionView.java
+++ TP10_SGBD_LOO/src/bdd/view/ConnexionView.java
@@ -16,25 +16,25 @@ public class ConnexionView {
 		Composite data = new Composite(composite, SWT.NONE);
 		data.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		data.setLayout(new GridLayout(2, false));
-		
+
 		Label numerolabel = new Label(data, SWT.NONE);
 		numerolabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		numerolabel.setText("Entrez votre Numéro de Sécurité Social :");
-		
+
 		Text entrernumlabel = new Text(data, SWT.SINGLE | SWT.LEAD | SWT.BORDER);
 		entrernumlabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		entrernumlabel.setText("");
-		
+
 		Button buttonvaliderlabel = new Button(data, SWT.PUSH);
 		buttonvaliderlabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		buttonvaliderlabel.setText("Connexion");
 		buttonvaliderlabel.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+
 			}
 		});
-		
+
 	}
 
 }
